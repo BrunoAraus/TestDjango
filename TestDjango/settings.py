9658e1f94124bcd1944cbb20ffdfe1666dabeb2b
BrunoAraus <101360453+BrunoAraus@users.noreply.github.com>
2022-06-14 21:30:09 -0400

    Pagina final

--- TestDjango/settings.py
+++ TestDjango/settings.py
@@ -78,10 +78,8 @@ WSGI_APPLICATION = 'TestDjango.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.oracle',
-        'NAME': 'localhost:1521/xe',
-        'USER':'django',
-        'PASSWORD': 'django',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
